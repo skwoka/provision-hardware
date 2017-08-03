0dc82ffb5a8c5afe9ef0b2f1f426222004ffe5a0
Stanley Kwok <skwoka@gmail.com>
2017-08-03 12:55:54 -0700

    Install rbenv and ruby-build

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,5 +8,25 @@ brew doctor
 # Install Caskroom
 brew tap caskroom/cask
 
+ADMIN_HOME=~/administration
+mkdir "$ADMIN_HOME"
+
 # Install macOSX-defaults
-git clone https://github.com/kevinSuttle/macOS-Defaults.git
+git clone https://github.com/kevinSuttle/macOS-Defaults.git "$ADMIN_HOME/macOSX-defaults"
+
+# Install rbenv
+if [ ! -d ".rbenv" ] ; then
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+fi
+
+cd ~/.rbenv && src/configure && make -C src
+
+which rbenv
+if [ $? != 0 ] ; then
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+fi
+
+~/.rbenv/bin/rbenv init
+
+# Install ruby build
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
