3437b8f30989f3a441e2e00f4a57efaa0d52fb83
Stanley Kwok <skwoka@gmail.com>
2017-08-03 13:05:35 -0700

    Fixed bootstrap script to ensure it is idempotent

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,24 +12,33 @@ ADMIN_HOME=~/administration
 mkdir "$ADMIN_HOME"
 
 # Install macOSX-defaults
-git clone https://github.com/kevinSuttle/macOS-Defaults.git "$ADMIN_HOME/macOSX-defaults"
+MACOS_DEFAULTS_HOME="$ADMIN_HOME/macOSX-defaults"
+if [ ! -d "$MACOS_DEFAULTS_HOME" ] ; then
+  git clone https://github.com/kevinSuttle/macOS-Defaults.git "$MACOS_DEFAULTS_HOME"
+else
+  cd "$MACOS_DEFAULTS_HOME" && git pull --rebase
+fi
 
 # Install rbenv
 if [ ! -d ".rbenv" ] ; then
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  cd ~/.rbenv && src/configure && make -C src
 fi
 
-cd ~/.rbenv && src/configure && make -C src
-
 which rbenv
-if [ $? != 0 ] ; then
+if [ $? -ne 0 ] ; then
   echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+else
+  source ~/.bash_profile
 fi
 
 ~/.rbenv/bin/rbenv init
 
 # Install ruby build
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+RUBY_BUILD_HOME=~/.rbenv/plugins/ruby-build
+if [ ! -d "$RUBY_BUILD_HOME" ] ; then
+  git clone https://github.com/rbenv/ruby-build.git "$RUBY_BUILD_HOME"
+fi
 
 # Install common ruby versions
 rbenv install 2.4.1
